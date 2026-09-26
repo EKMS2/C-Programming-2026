8603339b536087bd762971fb72f8691f88617a03
EKMS <atacasin@gmail.com>
2026-09-27 01:28:48 +0900

    Add files via upload

---
+++ 4주차 연산자 실습-합격 여부 판단-2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+	int score = 75, attendance = 85;
+	int passed = score >= 60 && attendance >= 80;
+	
+	printf("%d", passed);
+	
+	/*int score, attendance;
+	
+	scanf("%d %d", &score, &attendance); 
+	
+	if(score >= 60 && attendance >= 80)
+	{
+		printf("합격입니다.");
+	}
+	else
+	{
+		printf("불합격입니다.");
+	}
+	*/
+}
